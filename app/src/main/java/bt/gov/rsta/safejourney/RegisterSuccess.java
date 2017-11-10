1ffc4dc12abeb03ed19af3e935da83b684c67974
Tek Nath <nathtek136@gmail.com>
2017-11-10 10:49:19 +0600

    some changes to regristration.java

--- app/src/main/java/bt/gov/rsta/safejourney/RegisterSuccess.java
+++ app/src/main/java/bt/gov/rsta/safejourney/RegisterSuccess.java
@@ -6,15 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterSuccess extends AppCompatActivity {
-    EditText regsucess;
+    TextView regsucess;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_success);
         final Context regSucessContex = this;
+        regsucess=(TextView) findViewById(R.id.regSucess);
 
         regsucess.setOnClickListener(new View.OnClickListener() {
             @Override
