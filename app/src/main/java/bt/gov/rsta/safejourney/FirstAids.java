450d36d9fb9cdcea6a4855e8bdd0e976d185cb93
Tek Nath Acharya <31593478+nathtek136@users.noreply.github.com>
2017-11-10 20:20:58 +0600

    Update FirstAids.java

--- app/src/main/java/bt/gov/rsta/safejourney/FirstAids.java
+++ app/src/main/java/bt/gov/rsta/safejourney/FirstAids.java
@@ -7,36 +7,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-<<<<<<< HEAD
-class FractureMainActivity extends Activity {
-
-    ExpandableListAdapter listAdapter;
-    ExpandableListView expListView;
-    List<String> listDataHeader;
-    HashMap<String, List<String>> listDataChild;
-=======
 public class FirstAids extends AppCompatActivity {
->>>>>>> master
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-<<<<<<< HEAD
-        setContentView(R.layout.activity_main);
-
-=======
         setContentView(R.layout.activity_first_aids);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
->>>>>>> master
     }
 }
