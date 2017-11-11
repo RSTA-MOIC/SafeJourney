81a787b7ece2f8132899009192e566c6cf234d10
Sapna <sapnathapa@405gmail.com>
2017-11-10 23:33:29 -0800

    details

--- app/src/main/java/bt/gov/rsta/safejourney/FirstAid.java
+++ app/src/main/java/bt/gov/rsta/safejourney/FirstAid.java
@@ -7,7 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class FirstAids extends AppCompatActivity {
+class FirstAids extends AppCompatActivity {
 
 
     @Override
